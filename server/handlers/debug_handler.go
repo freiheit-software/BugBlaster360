adb7f3ec279f041c7b6d37a58ffb6a2471f27bad
rdunker <bm.rebekka@googlemail.com>
2023-06-17 01:52:32 +0200

    Move file parsing to debug handler

--- server/handlers/debug_handler.go
+++ server/handlers/debug_handler.go
@@ -2,8 +2,12 @@ package handlers
 
 import (
 	"context"
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/google/go-github/v53/github"
@@ -28,12 +32,23 @@ func HandleDebug(w http.ResponseWriter, r *http.Request) {
 		}
 	*/
 
+	var testRunObject = parseTestFile()
+	var title = "Test Results"
+	var summary = testRunObjectToSummary(testRunObject)
+	var text = testRunObjectToMarkdownTable(testRunObject)
+
 	status := "completed"
 	conclusion := "success"
+
 	options := github.UpdateCheckRunOptions{
 		Name:       "Custom Test Visualizer",
 		Status:     &status,
 		Conclusion: &conclusion,
+		Output: &github.CheckRunOutput{
+			Title:   &title,
+			Summary: &summary,
+			Text:    &text,
+		},
 	}
 
 	//checkrun, _, err := client.Checks.CreateCheckRun(ctx, "facilioo", "facilioo", options)
@@ -54,4 +69,83 @@ func HandleDebug(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Print(*app.Slug)
 	*/
+}
+
+type TestRun struct {
+	XMLName xml.Name `xml:"TestRun"`
+	Results Results  `xml:"Results"`
+}
+
+type Results struct {
+	XMLName         xml.Name         `xml:"Results"`
+	UnitTestResults []UnitTestResult `xml:"UnitTestResult"`
+}
+
+type UnitTestResult struct {
+	XMLName      xml.Name `xml:"UnitTestResult"`
+	ExecutionId  string   `xml:"executionId,attr"`
+	TestName     string   `xml:"testName,attr"`
+	ComputerName string   `xml:"computerName,attr"`
+	Duration     string   `xml:"duration,attr"`
+	Status       string   `xml:"outcome,attr"`
+}
+
+func parseTestFile() TestRun {
+	// Open our xmlFile
+	xmlFile, err := os.Open("testfile.trx")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("opened file")
+
+	// defer the closing of our xmlFile so that we can parse it later on
+	defer xmlFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(xmlFile)
+
+	// we initialize our TestRun entity
+	var out TestRun
+	// we unmarshal our byteArray which contains our
+	// xmlFiles content into 'results' which we defined above
+	xml.Unmarshal(byteValue, &out)
+
+	return out
+}
+
+func testRunObjectToMarkdownTable(testRun TestRun) string {
+	// parse to markdown string
+	var markdown = "| Test | Status | Duration | Computer |\n" +
+		"| ---- | ------ | -------- | -------- |\n"
+
+	for i := 0; i < len(testRun.Results.UnitTestResults); i++ {
+		var test = testRun.Results.UnitTestResults[i].TestName
+		var status = testRun.Results.UnitTestResults[i].Status
+		if status == "Passed" {
+			status = "Passed :heavy_check_mark:"
+		} else if status == "Failed" {
+			status = "Failed :x:"
+		}
+		var duration = testRun.Results.UnitTestResults[i].Duration
+		var computer = testRun.Results.UnitTestResults[i].ComputerName
+		markdown = markdown + "| " + test + " | " + status + " | " + duration + " | " + computer + " |\n"
+	}
+
+	return markdown
+}
+
+func testRunObjectToSummary(testRun TestRun) string {
+	var failingTests = 0
+	for i := 0; i < len(testRun.Results.UnitTestResults); i++ {
+		if testRun.Results.UnitTestResults[i].Status != "Passed" {
+			failingTests++
+		}
+	}
+	if failingTests == 0 {
+		return "All tests passed! :rocket:"
+	} else {
+		return strconv.Itoa(failingTests) + " tests did not pass. :eyes:"
+	}
 }
