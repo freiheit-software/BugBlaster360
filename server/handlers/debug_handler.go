644513e9593b70b2509533b0a02a927d69b984f9
Marc André Freiheit <marcandre@freiheit.software>
2023-06-17 01:15:19 +0200

    Add Basic Code for Check Handling

--- server/handlers/debug_handler.go
+++ server/handlers/debug_handler.go
@@ -15,14 +15,43 @@ func HandleDebug(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	itr, _ := ghinstallation.NewAppsTransportKeyFromFile(http.DefaultTransport, 348670, "key.pem")
+	ctx := context.Background()
+
+	itr, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, 348670, 38691856, "key2.pem")
+	//itr, _ := ghinstallation.NewAppsTransportKeyFromFile(http.DefaultTransport, 348670, "key2.pem")
 	client := github.NewClient(&http.Client{Transport: itr})
 
-	app, _, err := client.Apps.Get(context.Background(), "")
+	/*
+		options := github.CreateCheckRunOptions{
+			Name:    "Custom Test Visualizer",
+			HeadSHA: "bdfa19b89ffd80b29f07fb193d3bf60382159b1e",
+		}
+	*/
+
+	status := "completed"
+	conclusion := "success"
+	options := github.UpdateCheckRunOptions{
+		Name:       "Custom Test Visualizer",
+		Status:     &status,
+		Conclusion: &conclusion,
+	}
+
+	//checkrun, _, err := client.Checks.CreateCheckRun(ctx, "facilioo", "facilioo", options)
+	checkrun, _, err := client.Checks.UpdateCheckRun(ctx, "facilioo", "facilioo", 14335840736, options)
 
 	if err != nil {
 		fmt.Print(err.Error())
 	}
 
-	fmt.Print(*app.Slug)
+	fmt.Print(*checkrun.ID)
+
+	/*
+		app, _, err := client.Apps.Get(ctx, "")
+
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		fmt.Print(*app.Slug)
+	*/
 }
